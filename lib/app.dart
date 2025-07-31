bfc7c80eeb07d08e7e6d93812d3aa9170ac0d62f
Bishal Chandro Modak <bishalchandromodak@gmail.com>
2025-07-31 10:17:38 +0600

    add network caller

--- lib/app.dart
+++ lib/app.dart
@@ -17,6 +17,7 @@ class app extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       navigatorKey: navigator,
       theme: ThemeData(
         colorSchemeSeed: Colors.green,
